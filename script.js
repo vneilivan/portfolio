24abf59b40dd47d2339121406f5686fb3d7980f9
vneilivan <147904651+vneilivan@users.noreply.github.com>
2025-08-19 23:26:25 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -72,3 +72,18 @@ window.addEventListener("click", (e) => {
     modal.style.display = "none";
   }
 });
+
+
+// Reveal sections on scroll
+const sections = document.querySelectorAll("section");
+
+window.addEventListener("scroll", () => {
+  const triggerBottom = window.innerHeight * 0.8;
+
+  sections.forEach(section => {
+    const sectionTop = section.getBoundingClientRect().top;
+    if (sectionTop < triggerBottom) {
+      section.classList.add("visible");
+    }
+  });
+});
